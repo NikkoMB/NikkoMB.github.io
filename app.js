cc9e4ae08b90261b938409a7840bc0045e31a88a
bustillo.nikko@yahoo.com <bustillo.nikko@yahoo.com>
2019-10-07 21:25:59 -0400

    Changed whole website. w3

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(process.env.PORT || 9000, () => {
+    console.log('..LISTENING ON PORT 9000..');
+});
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/styles.css', express.static(path.join(__dirname, '/styles.css')));
+
+
+////////////////////////////////////////////////////////////////////
+
+app.get('/', (req,res) => { //HOME PAGE
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+ 
+app.get('/about.html', (req,res) => { //ABOUT PAGE
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/blog.html', (req,res) => { //BLOG PAGE
+    res.sendFile(path.join(__dirname, 'blog.html'));
+});
+
+app.get('/contact.html', (req,res) => { //CONTACT PAGE
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
